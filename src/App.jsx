77c3a3aaab725ed626f1b2c426c6e0be3573c8a2
JohnsonOdang <johnnyodanny@gmail.com>
2024-03-25 03:05:36 -0700

    Blogging site

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,11 @@
-import React, { useState } from 'react'
-
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-  "Improve your React skill",
-  "Contribute to open source"
-];
+import React from 'react'
 
 function App() {
-  const [step, setStep] = useState(1)
-  const [isOpen, setIsOpen] = useState(true)
-
-  function handlePrevious() {
-    if (step > 1) setStep((s) => s - 1);
-  }
-
-  function handleNext() {
-    if (step < 5) {
-      setStep((s) => s + 1);
-    }
-  }
-
   return (
     <>
-      <button className='close' onClick={() => setIsOpen((is) => !is)}>
-        &times;
-      </button>
-
-      {isOpen && (
-        <div className='steps'>
-          <div className='numbers'>
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
-            <div className={step >= 4 ? "active" : ""}>4</div>
-            <div className={step >= 5 ? "active" : ""}>5</div>
-          </div>
-
-          <p className='message'>
-            Step {}: {messages[step - 1]}
-          </p>
-
-          <div className='buttons'>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+      <h1>Hello World</h1>
     </>
-  );
+  )
 }
 
 export default App
---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+
+function Header() {
+  return (
+    <header className='home-header'>
+      <h2>Inc. This Morning</h2>
+      <h1>
+        <span>"<span>Blog</span>"</span>
+      </h1>
+      <p>Awesome place to make yourself <br /> productive and entertained through
+       daily updates.
+      </p>
+    </header>
+  )
+}
+
+export default Header
